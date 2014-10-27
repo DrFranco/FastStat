cc746b6dde729700a07c49eb5b516a8c43648e4c
Michael Franco <michaelfranco@Michaels-MacBook-Pro.local>
2014-10-26 21:37:01 -0700

    Initial Commit

---
+++ FastStat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FastStat
+//
+//  Created by Michael Franco on 10/26/14.
+//  Copyright (c) 2014 Michael Franco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FastStat/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FastStat
+//
+//  Created by Michael Franco on 10/26/14.
+//  Copyright (c) 2014 Michael Franco. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
