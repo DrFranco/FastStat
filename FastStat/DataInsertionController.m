429cc305facbc9d505e1f85f9639e97a21806cc4
DrFranco <michaelfranco@Michaels-MacBook-Pro.local>
2014-11-11 00:31:25 -0800

    Finished data insertion handling
    Haven’t checked to see if items are taking the values, but it’s running
    smoothly

--- FastStat/DataInsertionController.m
+++ FastStat/DataInsertionController.m
@@ -11,31 +11,48 @@
 
 @synthesize textField, currGame;
 
--(IBAction) buttonPushed
+-(IBAction) buttonPushed: (id) sender
 {
-    NSArray *temp = [currGame playerAccessor];
-    Player *tempPlayer = [temp objectAtIndex:[textField.text integerValue]];
     NSString *restorationId = self.restorationIdentifier;
-    if ([restorationId isEqualToString:@"Ejection Against"])
-        tempPlayer.ejectionsAgainst++;
-    else if([restorationId isEqualToString:@"Ejection Earned by"])
-        tempPlayer.ejectionsEarned++;
-    else if([restorationId isEqualToString:@"Game Played"])
-        tempPlayer.gamePlayed=YES;
-    else if([restorationId isEqualToString:@"Game Started"])
-        tempPlayer.gameStarted=YES;
-    else if([restorationId isEqualToString:@"Turnover by"])
-        tempPlayer.turnovers++;
-    else if([restorationId isEqualToString:@"Offensive by"])
-        tempPlayer.steals++;
-    else if([restorationId isEqualToString:@"Field Block by"])
-        tempPlayer.fieldBlocks++;
-    else if([restorationId isEqualToString:@"Shot by"])
-        tempPlayer.attempts++;
-    else if([restorationId isEqualToString:@"Goal by"])
-        tempPlayer.goals++;
-    else if([restorationId isEqualToString:@"Assist by"])
-        tempPlayer.assists++;
+    UIAlertView *alert= [[UIAlertView alloc] initWithTitle:[NSString stringWithFormat:@"%@ %@",restorationId,textField.text]message:@"Choose applicable" delegate: self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Confirm", nil];
+    [alert show];
 }
+- (void) alertView: (UIAlertView *) alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex==1)
+    {
+        NSArray *temp = [currGame playerAccessor];
+        Player *tempPlayer = [temp objectAtIndex:[textField.text integerValue]];
+        NSString *restorationId = self.restorationIdentifier;
+        if ([restorationId isEqualToString:@"Ejection Against"])
+            tempPlayer.ejectionsAgainst++;
+        else if([restorationId isEqualToString:@"Ejection Earned by"])
+            tempPlayer.ejectionsEarned++;
+        else if([restorationId isEqualToString:@"Game Played"])
+            tempPlayer.gamePlayed=YES;
+        else if([restorationId isEqualToString:@"Game Started"])
+            tempPlayer.gameStarted=YES;
+        else if([restorationId isEqualToString:@"Turnover by"])
+            tempPlayer.turnovers++;
+        else if([restorationId isEqualToString:@"Offensive by"])
+            tempPlayer.steals++;
+        else if([restorationId isEqualToString:@"Field Block by"])
+            tempPlayer.fieldBlocks++;
+        else if([restorationId isEqualToString:@"Shot by"])
+            tempPlayer.attempts++;
+        else if([restorationId isEqualToString:@"Goal by"])
+            tempPlayer.goals++;
+        else if([restorationId isEqualToString:@"Assist by"])
+            tempPlayer.assists++;
+        [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+    }
+}
+-(IBAction)cancelButton:(id)sender
+{
+    [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+
 
 @end
